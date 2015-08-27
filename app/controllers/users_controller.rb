879251b8a752246e64738e688a83af3b31915fad
Dustin Milam <darklordsushi@yahoo.com>
2015-08-27 18:16:44 -0400

    change form helpers to form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
 	def create
-		@user = User.new(name: params[:name], email: params[:email], password_digest: params[:password_digest])
-		# @user = User.new(user_params)
+		#@user = User.new(name: params[:name], email: params[:email], password_digest: params[:password_digest])
+		@user = User.new(user_params)
 		if @user.save
 			redirect_to new_user_path
 		else
 			render :new
 		end
+	end
+
+	def edit 
+
+	end
+
+	def update
+		@user = User.find(params[:id])
 	end
 
 	private
